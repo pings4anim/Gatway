5cf122434aa4e1190beeea7538dc57aad439dca2
Piyush <piyush.agilic@gmail.com>
2025-07-12 13:20:26 +0530

    correction in ocelot

--- Gatway/Program.cs
+++ Gatway/Program.cs
@@ -12,6 +12,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Add Ocelot
 builder.Services.AddOcelot();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policy =>
+        {
+            policy.WithOrigins("https://cicdnewui-g8fhccgyege2d2bp.canadacentral-01.azurewebsites.net")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline test.
@@ -20,11 +32,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MapGet("/version", () => "Deployed version: 1.0.1 - CORS updated-Piyush-CI-Gatway");
 app.UseHttpsRedirection();
-await app.UseOcelot();
+app.UseCors("AllowSpecificOrigin");
+
 app.UseAuthorization();
 
 app.MapControllers();
-
+await app.UseOcelot();
 app.Run();
